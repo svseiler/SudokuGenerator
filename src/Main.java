fae72976789d22ddea05a28cbfe80a799ff51039
Sven Seiler <98035180+svseiler@users.noreply.github.com>
2022-01-20 17:59:04 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,141 @@
-import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static int[][][] cube = new int[9][9][10];
+    public static int posX = 0;
+    public static int posY = 0;
 
-        General general = new General();
-        Generator generator = new Generator();
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        int randNumber = 0;
 
-        System.out.println("Wie viele Sudokus möchten Sie generieren?");
-        int anzahlSudkous = sc.nextInt() + 1;
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                randNumber = (int) (Math.random() * 9) + 1;
+                posX = x;
+                posY = y;
+                insertAllBlockedNums();
+                if (cube[x][y][randNumber] == 0) {
+                    cube[x][y][0] = randNumber;
+                } else {
+                    for (int i = 1; i < 10; i++) {
+                        if (cube[x][y][i] == 0) {
+                            i = 10;
+                            x -= 1;
+                        } else if (i == 9 && cube[x][y][i] != 0) {
+                            eraseNum();
+                            if (x == 0) {
+                                x = 7;
+                                y -= 1;
+                            } else {
+                                x -= 2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-        //Generiert die gewünschte Sudoku Anzahl
-        for (int i = 1; i < anzahlSudkous; i++) {
-            int[][][] cube = new int[9][9][13];
-            cube = generator.generateFullSudokuField(cube);
-            generator.generateFinalSuduokuField(cube);
-            System.out.println("Sudoku Feld(" + i + "):");
-            general.array2Dprint(cube, 10);
-            System.out.println("Lösung:");
-            general.array2Dprint(cube, 11);
+        array2Dprint();
+        clearCube();
+        array3Dprint();
+    }
+
+    public static void insertAllBlockedNums() {
+        int numRow = 0;
+        int numCol = 0;
+        int numSquare = 0;
+
+        int x = posX;
+        int y = posY;
+
+        if ((x + 1) % 3 == 0) {
+            x -= 2;
+        } else if (x == 1 || x == 4 || x == 7) {
+            x -= 1;
+        }
+
+        if ((y + 1) % 3 == 0) {
+            y -= 2;
+        } else if (y == 1 || y == 4 || y == 7) {
+            y -= 1;
+        }
+
+        for (int j = y; j < (y + 3); j++) {
+            for (int i = x; i < (x + 3); i++) {
+                numSquare = cube[i][j][0];
+                if (i == posX && j == posY) {
+                    i = 9;
+                    j = 9;
+                } else {
+                    cube[posX][posY][numSquare] = numSquare;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            numRow = cube[i][posY][0];
+            numCol = cube[posX][i][0];
+            if (numRow == 0 && numCol == 0) {
+                i = 8;
+            } else {
+                cube[posX][posY][numRow] = numRow;
+                cube[posX][posY][numCol] = numCol;
+            }
         }
     }
+
+    public static void eraseNum() {
+        for (int i = 0; i < 10; i++) {
+            cube[posX][posY][i] = 0;
+        }
+    }
+
+    public static void clearCube() {
+        for (int z = 1; z < 10; z++) {
+            for (int y = 0; y < 9; y++) {
+                for (int x = 0; x < 9; x++) {
+                    cube[x][y][z] = 0;
+                }
+            }
+        }
+    }
+
+    public static void array2Dprint() {
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                System.out.print("  ");
+                System.out.print(cube[x][y][0]);
+                if ((x + 1) % 3 == 0) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+            if ((y + 1) % 3 == 0) {
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static void array3Dprint() {
+        for (int z = 0; z < 10; z++) {
+            for (int y = 0; y < 9; y++) {
+                for (int x = 0; x < 9; x++) {
+                    System.out.print("  ");
+                    System.out.print(cube[x][y][z]);
+                    if ((x + 1) % 3 == 0) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.print("\n");
+                if ((y + 1) % 3 == 0) {
+                    System.out.print("\n");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+
 }
 
